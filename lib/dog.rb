992d901c31e7e16626716884c408bad0ce088948
Melissa Holt <melissa.holt21@gmail.com>
2019-11-21 01:53:35 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Dog
+  
+  def name=(dog_name)
+    @i_am_a_doggo = dog_name
+  end
+  
+  
+  def name
+    @i_am_a_doggo
+  end
+  
+  
+  def bark
+    puts "woof!"
+  
+  end
+  
+end
